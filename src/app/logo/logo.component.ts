36363a4663993074d50797134d9f589e2ca48695
Jonathan Schmitz <jomitz96@hotmail.com>
2025-07-02 10:53:18 +0200

    Clean Letter size ratios

--- src/app/logo/logo.component.ts
+++ src/app/logo/logo.component.ts
@@ -16,12 +16,12 @@ export class LogoComponent implements AfterViewInit{
   @ViewChildren('letterImg') letterImages!: QueryList<ElementRef>;
 
   letters = [
-    { src: 'svg/Joscco_0.svg', alt: "J", alternatives: ['svg/Note.svg'], default: 'svg/Joscco_0.svg' },
-    { src: 'svg/Joscco_1.svg', alt: "o", alternatives: ['svg/Zero.svg'], default: 'svg/Joscco_1.svg' },
-    { src: 'svg/Joscco_2.svg', alt: "s", alternatives: ['svg/Sigma.svg'], default: 'svg/Joscco_2.svg' },
-    { src: 'svg/Joscco_3.svg', alt: "c", alternatives: [], default: '/svg/Joscco_3.svg' },
-    { src: 'svg/Joscco_4.svg', alt: "c", alternatives: ['svg/KO.svg'], default: 'svg/Joscco_4.svg' },
-    { src: 'svg/Joscco_5.svg', alt: "o", alternatives: ['svg/Head.svg'], default: 'svg/Joscco_5.svg' }
+    { src: 'svg/joscco_0.svg', alt: "J", alternatives: ['svg/joscco_0_1.svg'], default: 'svg/joscco_0.svg' },
+    { src: 'svg/joscco_1.svg', alt: "o", alternatives: ['svg/joscco_1_1.svg'], default: 'svg/joscco_1.svg' },
+    { src: 'svg/joscco_2.svg', alt: "s", alternatives: ['svg/joscco_2_1.svg', 'svg/joscco_2_2.svg'], default: 'svg/joscco_2.svg' },
+    { src: 'svg/joscco_3.svg', alt: "c", alternatives: [], default: '/svg/joscco_3.svg' },
+    { src: 'svg/joscco_4.svg', alt: "c", alternatives: ['svg/joscco_4_1.svg'], default: 'svg/joscco_4.svg' },
+    { src: 'svg/joscco_5.svg', alt: "o", alternatives: ['svg/joscco_5_1.svg'], default: 'svg/joscco_5.svg' }
   ];
 
   private lastReplacedIndex: number | null = null;
@@ -43,17 +43,17 @@ export class LogoComponent implements AfterViewInit{
       if (this.lastReplacedIndex !== null) {
         const lastLetter = this.letters[this.lastReplacedIndex];
         const lastImgElement = this.letterImages.toArray()[this.lastReplacedIndex].nativeElement;
-        gsap.to(lastImgElement, { scale: 0, duration: 0.3, onComplete: () => {
+        gsap.to(lastImgElement, { scale: 0, duration: 0.15, onComplete: () => {
             lastLetter.src = lastLetter.default;
-            gsap.to(lastImgElement, { scale: 1, duration: 0.3 });
+            gsap.to(lastImgElement, { scale: 1, duration: 0.15 });
           }});
       }
 
       // Replace the current letter
       const imgElement = this.letterImages.toArray()[randomIndex].nativeElement;
-      gsap.to(imgElement, { scale: 0, delay: 0.1, duration: 0.3, onComplete: () => {
+      gsap.to(imgElement, { scale: 0, delay: 0.1, duration: 0.15, onComplete: () => {
           letter.src = newSrc;
-          gsap.to(imgElement, { scale: 1, duration: 0.3 });
+          gsap.to(imgElement, { scale: 1, duration: 0.15 });
         }});
 
       // Update the last replaced index
