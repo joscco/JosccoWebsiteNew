d158664c6b905f399e5fa94732a549620a83a271
Jonathan Schmitz <jomitz96@hotmail.com>
2025-07-06 19:38:32 +0200

    Kill tweens on retriggering animation

--- src/app/logo/logo.component.ts
+++ src/app/logo/logo.component.ts
@@ -3,6 +3,13 @@ import {RouterLink} from '@angular/router';
 import {NgForOf} from '@angular/common';
 import {gsap} from 'gsap';
 
+interface LetterData {
+  format: string;
+  default: string;
+  alt: string;
+  alternatives: string[];
+}
+
 @Component({
   selector: 'app-logo',
   imports: [
@@ -15,6 +22,8 @@ export class LogoComponent implements AfterViewInit {
 
   @ViewChildren('letterImg') letterImages!: QueryList<ElementRef>;
   @ViewChildren('altImg') altImages!: QueryList<ElementRef>;
+
+  private tweens: gsap.core.Tween[] = [];
 
   letters = [
     {format: 'svg/joscco_0_%s.svg', default: '0', alt: "J", alternatives: ['1', '2', '3', '4', '5']},
@@ -31,16 +40,17 @@ export class LogoComponent implements AfterViewInit {
 
   playAnimation() {
     const letterElements = this.letterImages.toArray();
+    this.tweens.forEach(tween => tween.kill()); // Stop any previous animations
+    this.tweens = [];
 
     // Randomly switch letters to alternatives
     letterElements.forEach((letterElement, index) => {
       const delays = index * 0.1; // Slight delay for each letter
-      const randomSwitches = 5; // Number of random switches per letter
       const letter = this.letters[index];
 
       this.shuffleArray(letter.alternatives).forEach((randomAlternative, i) => {
-        const src = letter.format.replace('%s', randomAlternative);
-        gsap.to(letterElement.nativeElement, {
+        const src = this.getSrcString(letter, randomAlternative);
+        let tween = gsap.to(letterElement.nativeElement, {
           scale: 0.9,
           delay: delays + i * 0.2,
           duration: 0.15,
@@ -49,18 +59,20 @@ export class LogoComponent implements AfterViewInit {
             gsap.to(letterElement.nativeElement, {scale: 1, duration: 0.15});
           }
         });
+        this.tweens.push(tween);
       });
 
       // Return to default at the end
-      gsap.to(letterElement.nativeElement, {
+      let finalTween = gsap.to(letterElement.nativeElement, {
         scale: 0.9,
-        delay: delays + randomSwitches * 0.2,
+        delay: delays + letter.alternatives.length * 0.2,
         duration: 0.15,
         onComplete: () => {
-          letterElement.nativeElement.src =  letter.format.replace('%s', letter.default);
+          letterElement.nativeElement.src =  this.getSrcString(letter);
           gsap.to(letterElement.nativeElement, {scale: 1, duration: 0.15});
         }
       });
+      this.tweens.push(finalTween);
     });
   }
 
@@ -71,5 +83,10 @@ export class LogoComponent implements AfterViewInit {
       [arr[i], arr[j]] = [arr[j], arr[i]];
     }
     return arr;
+  }
+
+  getSrcString(letter: LetterData, replacement?: string): string {
+    let replaceValue = replacement || letter.default;
+    return letter.format.replace('%s', replaceValue)
   }
 }
